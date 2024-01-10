1bf864c0034aa421ae62032ae50c662758674bbd
gh-johnny <jmfurtadoromero@gmail.com>
2024-01-10 17:25:57 -0300

    chore: initial structure

---
+++ src/Post.jsx
@@ -0,0 +1,12 @@
+
+export function Post(Props) {
+    return (
+        <>
+            <div>
+                <strong>{Props.author}</strong>
+                <p>{Props.content}</p>
+            </div>
+        </>
+    )
+}
+
