9db23b7876f3b057df59bc0619534251b2fa4ec1
gh-johnny <jmfurtadoromero@gmail.com>
2024-02-14 02:44:54 -0300

    workflow: deploy config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        port: 6464,
-    }
+    base: "/Feed-Website-React",
 })
